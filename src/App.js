a19a3a37c7de0aa6ad770e89137374ddeaf572cb
NeRo707 <tazo.gorgadze.02@gmail.com>
2025-03-07 20:24:47 +0400

    btn++

--- src/App.js
+++ src/App.js
@@ -1,42 +1,66 @@
-import './App.css';
-import React, {useState} from 'react';
+import "./App.css";
+import React, { useState } from "react";
 
 const App = () => {
-  const url = "https://api.quotable.io/random";
+  const url = "https://thequoteshub.com/api/random-quote";
   let quoteData = {
     content: "Let time be your only competitor.",
-    author: "Ahmed Saber"
-  }
-  const [quote, setQuote] = useState(quoteData)
+    author: "Ahmed Saber",
+  };
+  const [quote, setQuote] = useState(quoteData);
 
   const generateQuote = () => {
     fetch(url)
-      .then(response => response.json())
-      .then(data => {
-        console.log(data)
-        setQuote(data)
+      .then((response) => response.json())
+      .then((data) => {
+        console.log(data);
+        setQuote(data);
       });
-  }
+  };
 
   const copy = () => {
-    navigator.clipboard.writeText(quote.author + " once said: " + quote.content)
-    alert('copied')
-  }
+    navigator.clipboard.writeText(
+      quote.author + " once said: " + quote.content
+    );
+    alert("copied");
+  };
+
+  const shareToTwitter = () => {
+    window.open(
+      "https://twitter.com/intent/tweet?text=" +
+        quote.author +
+        " once said: " +
+        quote.text
+    );
+  };
+
+  const shareToWhatsApp = () => {
+    window.open(
+      "https://api.whatsapp.com/send?text=" +
+        quote.author +
+        " once said: " +
+        quote.text
+    );
+  };
+
 
   return (
     <>
       <h1>Quote Generator React App</h1>
       <div className="container">
-        <p>{quote.content}</p>
+        <p>{quote.text}</p>
         <span>{quote.author}</span>
         <div className="btns">
-          <button onClick={copy} className="btn">Copy</button>
+          <button onClick={copy} className="btn">
+            Copy
+          </button>
           <button onClick={generateQuote}>Generate Another Quote</button>
+          <button onClick={shareToTwitter}>Share to Twitter</button>
+          <button onClick={shareToWhatsApp}>Share to WhatsApp</button>
         </div>
       </div>
     </>
-  )
-}
-
+  );
+};
 
 export default App;
